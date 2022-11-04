3049384758d03119a333c54c0e69c08dea0f3406
Abidur-Rahman-Chowdhury <abid43iiuc@gmail.com>
2022-11-05 00:21:39 +0600

    setup routes

--- server/index.js
+++ server/index.js
@@ -3,22 +3,27 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/posts.js';
+
 const app = express();
 
-app.use(bodyParser.json({ limit: '30mb', extended: true }));
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL = `mongodb+srv://capturememories:uKj1GCJtd9yqjXBM@cluster0.43r3m3i.mongodb.net/?retryWrites=true&w=majority`;
 const PORT = process.env.PORT || 5000;
 
-mongoose
-  .connect(CONNECTION_URL)
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() =>
     app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
   )
-  .catch((error) => console.log(error.message));
+  .catch((error) => console.log(`${error} did not connect`));
 
-  app.get('/', (req, res) => {
-    res.send('The power tools server is running');
-  });
+ 
+  
